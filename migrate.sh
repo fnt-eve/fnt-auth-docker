22d026a257cfe23ac9ae0d21ddf0f76319f5f2d0
greg2010 <greg2008200@gmail.com>
2025-03-08 18:11:23 -0500

    skip db checks

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 . $VIRTUAL_ENV/bin/activate
-python ${AUTH_HOME}/myauth/manage.py migrate
+python ${AUTH_HOME}/myauth/manage.py migrate --skip-checks
 python ${AUTH_HOME}/myauth/manage.py collectstatic  --noinput
